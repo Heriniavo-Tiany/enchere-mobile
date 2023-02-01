7f03bb7a580b856164d953375b445459c691b83a
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2023-02-01 21:13:00 +0300

    photo upload & camera

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,33 +1,40 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
+import {usePhotoGallery} from "../components/Photo";
+
 
 const Page: React.FC = () => {
 
-  const { name } = useParams<{ name: string; }>();
+    const { name } = useParams<{ name: string; }>();
+    const {photos, takePhoto} = usePhotoGallery();
 
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>{name}</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+    return (
+        <>
+            <IonPage>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonButtons slot="start">
+                            <IonMenuButton />
+                        </IonButtons>
+                        <IonTitle>{name}</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
 
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">{name}</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name={name} />
-      </IonContent>
-    </IonPage>
-  );
+                <IonContent fullscreen>
+                    <IonHeader collapse="condense">
+                        <IonToolbar>
+                            <IonTitle size="large">{name}</IonTitle>
+                        </IonToolbar>
+                    </IonHeader>
+
+                    <IonButton onClick={() => takePhoto(1) }>Click</IonButton>
+
+
+                </IonContent>
+            </IonPage>
+        </>
+    );
 };
 
 export default Page;
